00468e1d0680c879a5f38a5afa492b95a87ec66a
fut0k0 <cccabell@icloud.com>
2019-04-11 10:18:12 -0700

    Passing lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each {|name| messages.push(badge_maker(name))}
+  messages
+end
+
+def assign_rooms(names)
+  assignments = []
+  counter = 1
+  names.each do |name|
+    assignments.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  assignments
+end
+
+def printer(names)
+  batch_badge_creator(names).each {|message| puts message}
+  assign_rooms(names).each {|assignment| puts assignment}
+end
