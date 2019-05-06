2454fe280567effee83d0d745bc63d70da518515
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 03:52:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def assign_rooms(attendees)
+  list = []
+  attendees.each do |attendee|
+    i = attendees.find_index(attendee) + 1
+    message = "Hello, #{attendee}! You'll be assigned to room #{i}!"
+    list.push(message)
+  end
+  list
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  badges.each do |badge|
+    puts badge
+  end
+  assignments = assign_rooms(attendees)
+  assignments.each do |assignment|
+    puts assignment
+  end
+end
+  
