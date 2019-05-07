c2276a5c555de1c7865a5478699b2eebfe7fd22d
Danielle Martinoli <daniechris5@gmail.com>
2019-05-07 17:25:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return badges
+end
+
+def assign_rooms(attendees)
+  return badges + room_assignments
+end
+
+def printer(attendees
+  return badges + room_assignments
+end
