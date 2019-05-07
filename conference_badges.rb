d3dcacbfad428ba3e69e22b5501d5aa44532795a
Danielle Martinoli <daniechris5@gmail.com>
2019-05-07 21:44:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,10 +8,10 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_rooms(attendees, rooms)
+def assign_rooms(attendees)
   attendees.each_with_index do |attendee, i|
-    room_assignments(attendee)
-  end
+    batch_badge_creator(attendee)
+    end
 end
 
 #def printer(attendees)
