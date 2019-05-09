ab249991c265a73ff5cffdfb945a3a848a7661ac
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 18:13:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  arr2 = []
+  arr.each do |i|
+    arr2 << "Hello, my name is #{i}."
+  end
+  return arr2
+end
+
+def assign_rooms(arr)
+  arr2 = []
+  count = 1
+  arr.each do |elem|
+    arr2 << "Hello, #{elem}! You'll be assigned to room #{count}!"
+    count += 1
+  end
+  return arr2
+end
+
+def printer(arr)
+  arr1 = batch_badge_creator(arr)
+  arr2 = assign_rooms(arr)
+  arr1.each do |n|
+    puts n
+  end
+  arr2.each do |n|
+    puts n 
+  end
+end
+
+
+
+
+
